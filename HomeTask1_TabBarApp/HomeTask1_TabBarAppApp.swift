e84c6de860fe06bae693ce98a5b6ff80f50579db
RadikN <radik.nuriev@revelsystems.com>
2022-03-05 21:00:54 +0300

    Initial Commit

---
+++ HomeTask1_TabBarApp/HomeTask1_TabBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeTask1_TabBarAppApp.swift
+//  HomeTask1_TabBarApp
+//
+//  Created by Radik Nuriev on 05.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeTask1_TabBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
